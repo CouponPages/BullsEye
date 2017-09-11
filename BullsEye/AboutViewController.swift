18a86ce59c70c0b45b5ce31e9cf76cf52fa6ac49
Joe Crescenzi <CouponPages@gmail.com>
2017-09-10 23:13:42 -0400

    Functionality is working... just needs a face-lift.

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Joe Crescenzi on 9/10/17.
+//  Copyright © 2017 Joe Crescenzi. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+	@IBAction func close()
+	{
+		dismiss(animated: true, completion: nil)
+	}
+	
+	
+	
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,29 +10,102 @@ import UIKit
 
 class ViewController: UIViewController {
 	
-	var CurrentValue: Int = 50
+	var CurrentValue = 0
+	var TargetValue = 0
+	var Score = 0
+	var Round = 0
 	
-	override func viewDidLoad() {
+	@IBOutlet weak var slider: UISlider!
+	@IBOutlet weak var TargetLabel: UILabel!
+	@IBOutlet weak var RoundLabel: UILabel!
+	@IBOutlet weak var ScoreLabel: UILabel!
+	
+	//
+	//
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		StartNewGame()
+		UpdateLabels()
 	}
 	
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	//
+	//
+	@IBAction func StartNewGame()
+	{
+		Score = 0
+		Round = 0
+		StartNewRound()
+		UpdateLabels()
 	}
 	
 	
+	//
+	//
+	func StartNewRound()
+	{
+		Round += 1
+		TargetValue = 1 + Int(arc4random_uniform(100))
+		CurrentValue = 50
+		slider.value = Float(CurrentValue)
+	}
+	
+	//
+	//
+	func UpdateLabels()
+	{
+		TargetLabel.text = String(TargetValue)
+		ScoreLabel.text = String(Score)
+		RoundLabel.text = String(Round)
+	}
+	
+	
+	//
+	//
 	@IBAction func showAlert()
 	{
-		let message = "The slider is \(CurrentValue)"
-		let alert = UIAlertController(title: "Hello World!", message: message, preferredStyle: .alert)
-		let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+		let Difference = abs(TargetValue - CurrentValue)
+		var Points = 100 - Difference
+
+		let Title: String
+		if Difference == 0
+		{
+			Title = "Bulls Eye!"
+			Points += 100
+		}
+		else if Difference < 5
+		{
+			Title = "Missed by \(Difference)\nYou ALMOST Had It!"
+			if Difference == 1
+			{
+				Points += 50
+			}
+			
+		}
+		else if Difference < 10
+		{
+			Title = "Missed by \(Difference)\nNot Bad"
+		}
+		else
+		{
+			Title = "Missed by \(Difference)\nNot Even Close..."
+		}
+		Score += Points
+		let message = "You Selected \(CurrentValue)\nYou Scored \(Points) Points."
+		let alert = UIAlertController(title: Title, message: message, preferredStyle: .alert)
+		let action = UIAlertAction(title: "OK", style: .default, handler: {
+					action in
+					self.StartNewRound()
+					self.UpdateLabels()
+					})
 		alert.addAction(action)
 		present(alert, animated: true, completion: nil)
+		// StartNewRound()
+		// UpdateLabels()
 	}
 	
-	
+	//
+	//
 	@IBAction func sliderMoved(_ slider: UISlider)
 	{
 			CurrentValue = lroundf(slider.value)
@@ -40,7 +113,14 @@ class ViewController: UIViewController {
 	}
 	
 	
-	
+	//
+	//
+	override func didReceiveMemoryWarning()
+	{
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
 	
 	
 	
